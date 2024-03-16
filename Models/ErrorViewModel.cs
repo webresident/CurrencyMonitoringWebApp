162b9ad9f19c934aa28b91703ff33850d8de862f
webresident <89592473+webresident@users.noreply.github.com>
2024-03-16 22:14:40 +0200

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurrencyMonitoringWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
